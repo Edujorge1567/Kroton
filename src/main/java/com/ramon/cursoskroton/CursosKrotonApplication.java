e1b52bfd1f20425935dce6c5b6d6e426a85d4ac7
Edujorge1567 <71086785+Edujorge1567@users.noreply.github.com>
2020-11-12 16:24:07 -0300

    Inicio do projeto

---
+++ src/main/java/com/ramon/cursoskroton/CursosKrotonApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.cursoskroton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosKrotonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosKrotonApplication.class, args);
+	}
+
+}
